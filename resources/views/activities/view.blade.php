e3fe0451504621c2a0b9cf7f90ce00418c3b0f02
scott <sbwilkinson@yahoo.com>
2018-05-25 17:18:43 -0700

    Added activity submenu component

--- resources/views/activities/view.blade.php
+++ resources/views/activities/view.blade.php
@@ -20,20 +20,10 @@ foreach($photos as $photo)
 
 	@guest
 	@else
-		@if (Auth::user()->user_type >= 1000)
-		<div class="" style="font-size:20px;">
-		<table class=""><tr>			
-			<td style="width:40px;"><a href='/activities/indexadmin/'><span class="glyphCustom glyphicon glyphicon-list"></span></a></td>
-			<td style="width:40px;"><a href='/activities/add/'><span class="glyphCustom glyphicon glyphicon-plus-sign"></span></a></td>
-			<td style="width:40px;"><a href='/activities/edit/{{$record->id}}'><span class="glyphCustom glyphicon glyphicon-edit"></span></a></td>
-			<td style="width:40px;"><a href='/photos/tours/{{$record->id}}'><span class="glyphCustom glyphicon glyphicon-picture"></span></a></td>
-			<td style="width:40px;"><a href='/activities/location/{{$record->id}}'><span class="glyphCustom glyphicon glyphicon-map-marker"></span></a></td>
-			<td style="width:40px;"><a href='/activities/publish/{{$record->id}}'><span class="glyphCustom glyphicon glyphicon-flash"></span></a></td>
-			<td style="width:40px;"><a href='/activities/confirmdelete/{{$record->id}}'><span class="glyphCustom glyphicon glyphicon-trash"></span></a></td>
-			
-		</tr></table>
-		</div>
-		@endif
+	
+		@component('menu-submenu-activities', ['record_id' => $record->id, 'record_title' => $record->title])
+		@endcomponent
+		
 		@if (Auth::user()->user_type >= 1000 || Auth::user()->id === $record->user_id)
 		<div class="publish-pills">
 			<ul class="nav nav-pills">
