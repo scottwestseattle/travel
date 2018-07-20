f5edcb1706c4fc0864f9dc439245801a978b4843
scott <sbwilkinson@yahoo.com>
2018-07-20 21:10:03 +0800

    Implemented photo type

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -52,7 +52,10 @@ class PhotoController extends Controller
 			}
 				
 			$vdata = $this->getViewData([
-				'title' => 'Tour', 'id' => $id, 'path' => $path, 'photos' => $photos, 'record_id' => $id
+				'id' => $id, 
+				'path' => $path, 
+				'photos' => $photos, 
+				'record_id' => $id,
 			]);
 				
 			return view('photos.index', $vdata);
@@ -63,45 +66,37 @@ class PhotoController extends Controller
         }
 	}
 
-	public function entries($parent_id)
+	public function entries($parent_id, $type_flag = PHOTO_TYPE_ENTRY)
 	{			
 		if (!$this->isAdmin())
              return redirect('/');
 
 		$parent_id = intval($parent_id);		 
 		
-    	if ($parent_id > 0 && Auth::check())
+    	if ($parent_id > 0)
         {
-			$subfolder = PHOTO_ENTRY_FOLDER . '/' . $parent_id . '/';			
+			$info = Controller::getPhotoInfo($type_flag);
+			$folder = $info['folder'];
+			$type = $info['type'];
+
+			$subfolder = $folder . '/' . $parent_id . '/';			
 			$path = $this->getPhotosWebPath($subfolder);
-			
+				
 			$photos = Photo::select()
 				//->where('site_id', SITE_ID)
-				->where('user_id', '=', Auth::id())
-				->where('deleted_flag', '<>', 1)
+				//->where('user_id', '=', Auth::id())
+				->where('deleted_flag', 0)
 				->where('parent_id', '=', $parent_id)
 				->orderByRaw('photos.main_flag DESC, photos.created_at ASC')
 				->get();
 				
-			if (false)
-			foreach($photos as $photo)
-			{
-				$fullPath = $this->getPhotosFullPath($subfolder) . $photo->filename;
-				
-				try
-				{
-					$size = filesize($fullPath);
-					$photo['size'] = $size;
-				}
-				catch (\Exception $e) 
-				{
-					$request->session()->flash('message.level', 'danger');
-					$request->session()->flash('message.content', $e->getMessage());
-				}
-			}
-				
 			$vdata = $this->getViewData([
-				'id' => $parent_id, 'path' => $path, 'photos' => $photos, 'record_id' => $parent_id			
+				'id' => $parent_id, 
+				'path' => $path, 
+				'photos' => $photos, 
+				'record_id' => $parent_id,
+				'type_flag' => $type_flag,
+				'type' => $type,
 			]);				
 				
 			return view('photos.index', $vdata);
@@ -132,8 +127,7 @@ class PhotoController extends Controller
 		$records = DB::select($q, [SITE_ID]);
 		
 		$vdata = $this->getViewData([
-			'title' => 'Slider', 
-			'photos' => $records, 		
+			'photos' => $records, 
 		]);
 						
 		return view('photos.sliders', $vdata);	
@@ -154,7 +148,7 @@ class PhotoController extends Controller
 			
 			$vdata = $this->getViewData([
 				'photos' => $photos,
-				'path' => $this->getPhotoPath(),
+				'path' => Controller::getPhotoPath(),
 			]);
 			
 			return view('photos.indexadmin', $vdata);
@@ -165,205 +159,219 @@ class PhotoController extends Controller
         }
     }
 		
-    public function add($parent_id = 0)
-    {
+    public function add($type_flag, $parent_id = 0)
+    {		
 		if (!$this->isAdmin())
 			return redirect('/');
-
-    	if (Auth::check())
-        {            
-			$vdata = $this->getViewData([
-				'parent_id' => $parent_id,
-			]);
-			
-			return view('photos.add', $vdata);
-        }           
-        else 
-		{
-			return redirect('/');
-        }       
+           
+		$type = Controller::getPhotoInfo($type_flag)['type'];
+		
+		$vdata = $this->getViewData([
+			'parent_id' => $parent_id,
+			'type_flag' => $type_flag,
+			'type' => $type,
+		]);
+		
+		return view('photos.add', $vdata);      
 	}
 	
     public function create(Request $request)
     {
 		if (!$this->isAdmin())
              return redirect('/');
-	
-    	if (Auth::check())
-        {            
-			//
-			// get file to upload
-			//
-			$file = $request->file('image');
-			
-			if (!isset($file))
-			{
-				$request->session()->flash('message.level', 'danger');
-				$request->session()->flash('message.content', 'Image to upload must be set using the [Browse] button');		
-				
-				$vdata = $this->getViewData([
-					'parent_id' => $request->parent_id,
-				]);
 
-				return view('photos.add', $vdata);
-			}
-			
-			//
-			// get and check file extension
-			//
-			$ext = strtolower($file->getClientOriginalExtension());
-			if (isset($ext) && $ext === 'jpg')
-			{
-				// correct extension
-			}
-			else
-			{
-				// bad or missing extension
-				$request->session()->flash('message.level', 'danger');
-				$request->session()->flash('message.content', 'Only JPG images can be uploaded');	
-				
-				return view('photos.add', ['parent_id' => $request->parent_id]);					
-			}
-						
-			//
-			// get and check new file name
-			//
-			$filename = $this->getPhotoName(trim($request->filename), $file->getClientOriginalName(), $alt_text_default);
-			
-			//
-			// get and fix alt_text
-			//
-			$alt_text = trim($request->alt_text);
-			if (isset($alt_text) && strlen($alt_text) > 0)
-			{
-				// alt_text is set
-			}
-			else
-			{
-				// alt_text not set, fix it up
-				if (isset($alt_text_default) && strlen($alt_text_default) > 0)
-				{
-					$alt_text = $alt_text_default;
-				}
-				else
-				{
-					// alt_text_default not set, so use filename to gen alt_text
-					$alt_text = str_replace("-", " ", $filename);	// replace dashes with spaces
-					$alt_text = str_replace(".jpg", "", $alt_text);	// remove file extension
-				}
-			}				
-				
-			$id = intval($request->parent_id);
-			
-			if ($id === 0) // for now these are sliders
-			{
-				// slider photos
-				$path = $this->getPhotosFullPath(PHOTO_SLIDER_FOLDER . '/');
-				
-				$redirect = '/photos/' . PHOTO_SLIDER_FOLDER;
-				$redirect_error = '/photos/add/0';
-			}
-			else
-			{
-				// tour photos
-				$path = $this->getPhotosFullPath(PHOTO_ENTRY_FOLDER . '/' . $id . '/');
-
-				$redirect = '/photos/' . PHOTO_ENTRY_FOLDER . '/' . $id;
-				$redirect_error = '/photos/add/' . $id;				
-			}
-						
-			try 
-			{
-				$tempPath = $path . PHOTO_TMP_FOLDER . '/';
-				if (!is_dir($tempPath)) 
-				{
-					$image_folder = $this->getPhotosFullPath(PHOTO_ENTRY_FOLDER);
-					if (!is_dir($image_folder))
-					{
-						// make the main folder
-						mkdir($image_folder, 0755);						
-					}
-
-					if (!is_dir($path))
-					{
-						// make the entry folder
-						mkdir($path, 0755);
-					}					
-					
-					mkdir($tempPath, 0755);// make the tmp folder with read/execute for everybody
-				}
+		$type = Controller::getPhotoInfo($request->type_flag)['type'];
+			 
+		$vdata = $this->getViewData([
+			'parent_id' => $request->parent_id,
+			'type_flag' => $request->type_flag,
+			'type' => $type,
+		]);	
 		
-				// upload the file
-				$request->file('image')->move($tempPath, $filename);
-				
-				//
-				// check the file size in case it needs to be reduced
-				//
-				$newSize = 0;
-				$size = filesize($tempPath . $filename);
-				$resized = false;
-				if ($id > 0 && intval($size) > 2000000) // 2mb limit for non-sliders only
-				{
-					// resize and put it up in the live photo folder
-					if ($this->resizeImage($tempPath, $path, $filename, /* new size = */ 750, /* makeOnly = */ true))
-					{
-						$resized = true;
-						$newSize = filesize($path . $filename);
-						unlink($tempPath . $filename); // delete the oversized file
-					}
-					else
-					{
-						$request->session()->flash('message.level', 'danger');
-						$request->session()->flash('message.content', 'Resizing of image failed');
-					}
-				}
-				else
-				{
-					// no need to resize, just move it from temp to live photo folder
-					rename($tempPath . $filename, $path . $filename);
-				}
-								
-				// add the photo record
-				$photo = new Photo();
-				$photo->site_id = SITE_ID;
-				$photo->filename = $filename;
-				$photo->alt_text = $alt_text;
-				$photo->location = trim($request->location);
-				$photo->main_flag = isset($request->main_flag) ? 1 : 0;
-				$photo->parent_id = $id;
-				$photo->user_id = Auth::id();
-				
-				//dd($photo);		
-				
-				$photo->save();
-				
-				$request->session()->flash('message.level', 'success');	
-				
-				if ($resized)
-					$request->session()->flash('message.content', 'Photo was successfully uploaded and resized from ' . number_format($size) . ' bytes to ' . number_format($newSize) . ' bytes');
-				else
-					$request->session()->flash('message.content', 'Photo was successfully uploaded');
-				
-				return redirect($redirect);
-			}
-			catch (\Exception $e) 
-			{
-				if ($e->getCode() == 0)
-				{
-				}
-				$request->session()->flash('message.level', 'danger');
-				$request->session()->flash('message.content', $e->getMessage());
-				
-				return redirect($redirect_error);
-			}					
-        }           
-        else 
+ 		//
+		// get file to upload
+		//
+		$file = $request->file('image');
+			
+		if (!isset($file))
 		{
-             return redirect('/');
-        }            	
+			$request->session()->flash('message.level', 'danger');
+			$request->session()->flash('message.content', 'Image to upload must be set using the [Browse] button');		
+			
+			return view('photos.add', $vdata);
+		}
+		
+		//
+		// get and check file extension
+		//
+		$ext = strtolower($file->getClientOriginalExtension());
+		if (isset($ext) && $ext === 'jpg')
+		{
+			// correct extension
+		}
+		else
+		{
+			// bad or missing extension
+			$request->session()->flash('message.level', 'danger');
+			$request->session()->flash('message.content', 'Only JPG images can be uploaded');	
+
+			return view('photos.add', $vdata);					
+		}
+					
+		//
+		// get and check new file name
+		//
+		$filename = $this->getPhotoName(trim($request->filename), $file->getClientOriginalName(), $alt_text_default);
+		
+		//
+		// get and fix alt_text
+		//
+		$alt_text = trim($request->alt_text);
+		if (isset($alt_text) && strlen($alt_text) > 0)
+		{
+			// alt_text is set
+		}
+		else
+		{
+			// alt_text not set, fix it up
+			if (isset($alt_text_default) && strlen($alt_text_default) > 0)
+			{
+				$alt_text = $alt_text_default;
+			}
+			else
+			{
+				// alt_text_default not set, so use filename to gen alt_text
+				$alt_text = str_replace("-", " ", $filename);	// replace dashes with spaces
+				$alt_text = str_replace(".jpg", "", $alt_text);	// remove file extension
+			}
+		}				
+			
+		$id = intval($request->parent_id);
+		
+		$type_flag = isset($request->type_flag) ? intval($request->type_flag) : PHOTO_TYPE_NOTSET;
+		$redirect = '/photos';
+		$redirect_error = '/photos/add';
+		$path = '/img/';
+		$folder = '';
+		
+		switch($type_flag)
+		{
+			case PHOTO_TYPE_SLIDER:
+				$path = $this->getPhotosFullPath(PHOTO_SLIDER_FOLDER . '/');
+				$redirect = '/photos/' . PHOTO_SLIDER_FOLDER;
+				$redirect_error = '/photos/add/' . PHOTO_SLIDER_FOLDER;
+				$folder = PHOTO_SLIDER_FOLDER;
+				break;
+			case PHOTO_TYPE_ENTRY:
+				$path = $this->getPhotosFullPath(PHOTO_ENTRY_FOLDER . '/' . $id . '/');
+				$redirect = '/photos/' . PHOTO_ENTRY_FOLDER . '/' . $id . '/' . $type_flag;
+				$redirect_error = '/photos/add/' . PHOTO_ENTRY_FOLDER . '/' . $id;				
+				$folder = PHOTO_ENTRY_FOLDER;
+				break;
+			case PHOTO_TYPE_RECEIPT:
+				$path = $this->getPhotosFullPath(PHOTO_RECEIPT_FOLDER . '/' . $id . '/');
+				$redirect = '/photos/' . PHOTO_ENTRY_FOLDER . '/' . $id . '/' . $type_flag;
+				$redirect_error = '/photos/add/' . PHOTO_RECEIPT_FOLDER . '/' . $id;				
+				$folder = PHOTO_RECEIPT_FOLDER;
+				break;
+			default:
+				break;
+		}
+					
+		try 
+		{
+			$tempPath = $path . PHOTO_TMP_FOLDER . '/';
+			if (!is_dir($tempPath)) 
+			{
+				$image_folder = $this->getPhotosFullPath($folder);
+				if (!is_dir($image_folder))
+				{
+					// make the main folder
+					mkdir($image_folder, 0755);						
+				}
+
+				if (!is_dir($path))
+				{
+					// make the entry folder
+					mkdir($path, 0755);
+				}					
+				
+				mkdir($tempPath, 0755);// make the tmp folder with read/execute for everybody
+			}
+	
+			// upload the file
+			$request->file('image')->move($tempPath, $filename);
+			
+			//
+			// check the file size in case it needs to be reduced
+			//
+			$newSize = 0;
+			$size = filesize($tempPath . $filename);
+			$resized = false;
+	
+			$filenameUnique = Controller::getUniqueFilename($path, $filename);
+			$duplicate = ($filenameUnique != $filename); // filename had to be changed to make it unique
+			
+			if ($id > 0 && intval($size) > 2000000) // 2mb limit for non-sliders only
+			{
+				// resize and put it up in the live photo folder
+				if ($this->resizeImage($tempPath, $path, $filename, $filenameUnique, /* new size = */ 750))
+				{
+					$resized = true;
+					$newSize = filesize($path . $filename);
+					unlink($tempPath . $filename); // delete the oversized file
+				}
+				else
+				{
+					$request->session()->flash('message.level', 'danger');
+					$request->session()->flash('message.content', 'Resizing of image failed');
+				}
+			}
+			else
+			{
+				// no need to resize, just move it from temp to live photo folder
+				rename($tempPath . $filename, $path . $filenameUnique);
+			}
+			
+			$filename = $filenameUnique;
+							
+			// add the photo record
+			$photo = new Photo();
+			$photo->site_id = SITE_ID;
+			$photo->filename = $filename;
+			$photo->alt_text = $alt_text;
+			$photo->location = trim($request->location);
+			$photo->main_flag = isset($request->main_flag) ? 1 : 0;
+			$photo->parent_id = $id;
+			$photo->user_id = Auth::id();
+			$photo->type_flag = $type_flag;
+						
+			$photo->save();
+			
+			$msgDuplicate = ($duplicate) ? 'DUPLICATE ' : '';
+
+			$request->session()->flash('message.level', $msgDuplicate ? 'danger' : 'success');	
+						
+			if ($resized)
+				$request->session()->flash('message.content', $msgDuplicate . 'Photo was successfully uploaded and resized from ' . number_format($size) . ' bytes to ' . number_format($newSize) . ' bytes');
+			else
+				$request->session()->flash('message.content', $msgDuplicate . 'Photo was successfully uploaded');
+			
+			return redirect($redirect);
+		}
+		catch (\Exception $e) 
+		{
+			if ($e->getCode() == 0)
+			{
+			}
+			$request->session()->flash('message.level', 'danger');
+			$request->session()->flash('message.content', $e->getMessage());
+			
+			return redirect($redirect_error);
+		}					
     }
 	
-	private function resizeImage($fromPath, $toPath, $filename, $heightNew, $makeOnly)
+	private function resizeImage($fromPath, $toPath, $filename, $filenameTo, $heightNew)
 	{
 		if (!is_dir($toPath)) 
 			mkdir($toPath, 0755);// make the folder with read/execute for everybody
@@ -374,17 +382,10 @@ class PhotoController extends Controller
 		//Debugger::dump('from: ' . $toPath);die;	
 			
 		$file = $fromPath;
+						
+		$file = Controller::appendPath($file, $filename);
+		$fileThumb = Controller::appendPath($toPath, $filenameTo);
 				
-		if (!$this->endsWith($fromPath, '/'))
-			$file .= '/';
-		
-		$file .= $filename;
-		
-		$fileThumb = $toPath . '/' . $filename;
-		
-		if ($makeOnly && file_exists($fileThumb))
-			return false; 
-			
 		$image_info = getimagesize($file);
 		
 		switch($image_info["mime"])
@@ -436,9 +437,9 @@ class PhotoController extends Controller
 			$width = imagesx($image) * $ratio; 			
 		}
 
-		$fileThumb = $toPath . '/' . $filename;
+		//sbw $fileThumb = $toPath . '/' . $filename;
 
-		if (file_exists($fileThumb))
+		if (false /*sbw*/ && file_exists($fileThumb))
 		{			
 			// check the thumb
 			$image_info_thumb = getimagesize($fileThumb);
@@ -556,7 +557,7 @@ class PhotoController extends Controller
     {
 		$this->saveVisitor(LOG_MODEL, LOG_PAGE_VIEW, $photo->id);
 
-		$path = $this->getPhotoPath($photo);
+		$path = Controller::getPhotoPath($photo);
 		
 		$vdata = $this->getViewData([
 			'photo' => $photo, 
@@ -573,20 +574,13 @@ class PhotoController extends Controller
              return redirect('/');
 		 
     	if ($this->isOwnerOrAdmin($photo->user_id))
-        {			
-			$path = '';
-			if (intval($photo->parent_id) === 0)
-			{
-				$path = '/img/' . PHOTO_SLIDER_FOLDER . '/' . $photo->filename;
-			}
-			else
-			{
-				$path = '/img/' . PHOTO_ENTRY_FOLDER . '/' . $photo->parent_id . '/' . $photo->filename;
-			}
+        {
+			$path = Controller::getPhotoPath($photo);
 
 			$vdata = $this->getViewData([
 				'record' => $photo, 
 				'path' => $path,
+				'type' => Controller::getPhotoInfo($photo->type_flag)['type'],
 			]);		
 			
 			return view('photos.edit', $vdata);
@@ -598,24 +592,18 @@ class PhotoController extends Controller
     }
 	
     public function update(Request $request, Photo $photo)
-    {
+    {		
 		if (!$this->isAdmin())
              return redirect('/');
-	
+			 
     	if ($this->isOwnerOrAdmin($photo->user_id))
         {
-			$id = intval($request->parent_id);
-			if ($this->isSlider($photo))
-			{
-				$path_from = base_path() . '/public/img/' . PHOTO_SLIDER_FOLDER . '/';
-				$redirect = '/photos/' . PHOTO_SLIDER_FOLDER . '/';
-			}
-			else
-			{
-				$path_from = base_path() . '/public/img/' . PHOTO_ENTRY_FOLDER . '/' . $id . '/';
-				$redirect = '/photos/' . PHOTO_ENTRY_FOLDER . '/' . $id;
-			}
-
+			$id = intval($photo->parent_id);
+			$info = Controller::getPhotoInfoPath($photo);
+			$folder = $info['folder'];			
+			$redirect = $info['redirect'];
+			$path_from = $info['filepath'];
+			
 			$filename = trim($request->filename);
 			
 			if ($request->filename_orig === $filename)
@@ -630,13 +618,18 @@ class PhotoController extends Controller
 					// file name changed, change the physical file name
 					//					
 					$path_to = $path_from;
-					
-					$path_from .= $request->filename_orig;
-					
+										
 					// get and fix up the new file name
 					$filename = $this->getPhotoName($filename, $request->filename_orig, $alt_text_default);
-					$path_to .= $filename;
-
+					
+					// check for duplicate filesname and create a unique name if necessary
+					$filenameUnique = Controller::getUniqueFilename($path_to, $filename);
+					$duplicate = ($filenameUnique != $filename); // filename had to be changed to make it unique
+					$filename = $filenameUnique;
+					
+					$path_from = Controller::appendPath($path_from, $request->filename_orig);
+					$path_to = Controller::appendPath($path_to, $filename);
+					
 					rename($path_from, $path_to);
 				}
 				else
@@ -693,14 +686,13 @@ class PhotoController extends Controller
 	
     	if ($this->isOwnerOrAdmin($photo->user_id))
         {			
-			$path = '';
-			if ($this->isSlider($photo))
-				$path .= '/img/' . PHOTO_SLIDER_FOLDER . '/';
-			else
-				$path .= '/img/' . PHOTO_ENTRY_FOLDER . '/' . $photo->parent_id . '/';
-	
+			$info = Controller::getPhotoInfoPath($photo);
+			$path = $info['path'];
+			
 			$vdata = $this->getViewData([
-				'photo' => $photo, 'path' => $path,
+				'photo' => $photo, 
+				'path' => $path,
+				'type' => $info['type'],
 			]);
 	
 			return view('photos.confirmdelete', $vdata);
@@ -713,11 +705,14 @@ class PhotoController extends Controller
 	
     public function delete(Request $request, Photo $photo)
     {		
+		if (!$this->isAdmin())
+             return redirect('/');
+
 		$redirect = null;
 		$message = null;
 		$messageLevel = null;
 		
-		$this->deletePhoto($photo, $redirect, $message, $messageLevel);
+		Controller::deletePhoto($photo, $redirect, $message, $messageLevel);
 		
 		$request->session()->flash('message.level', $messageLevel);
 		$request->session()->flash('message.content', $message);
@@ -731,8 +726,6 @@ class PhotoController extends Controller
 
 		//define image path
 		$path = $path . $photo->filename;
-
-		//dd($image);
 
 		// Load the image
 		$image = imagecreatefromjpeg($path);
