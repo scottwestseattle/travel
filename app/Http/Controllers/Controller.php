93cc543dd01b2d8271e3382bd0e8fde585fff289
scott <sbwilkinson@yahoo.com>
2018-09-26 21:20:15 +0600

    Added more todo items to admin

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -396,6 +396,7 @@ class Controller extends BaseController
 		
 		// orig: https://www.google.com/search?q=$1.$2,$3.$4
 		$link = 'https://maps.google.com/maps?q=$1.$2,$3.$4'; //new
+		$text = preg_replace('/Map Location: ([0-9]*+)\.([0-9]*+), ([0-9]*+)\.([0-9]*+)/i', '<a target="_blank" href="' . $link . '">Map Location: $1.$2, $3.$4</a>&nbsp;<a href="#"><span class="glyphicon glyphicon-copy" style="font-size:.7em;"></span></a>', $text);
 				
 		return $text;
 	}
