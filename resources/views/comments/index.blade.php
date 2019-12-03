11879bfe728b2d5d6075a33436c9760d7b1c2b39
Scott <sbwilkinson@yahoo.com>
2019-12-03 08:18:01 +0100

    Translated FP and Comments page dates

--- resources/views/comments/index.blade.php
+++ resources/views/comments/index.blade.php
@@ -16,7 +16,7 @@
 			<tr class="drop-box" style="vertical-align:middle; box-shadow: 2px 2px 2px 2px rgba(0, 0, 0, 0.2), 0 1px 1px 0 rgba(0, 0, 0, 0.19);">
 				<td style="min-width:100px; font-size: 1.5em; padding:10px; color: white; background-color: #74b567; margin-bottom:10px;" >
 					<div style="margin:0; padding:0; line-height:100%;">
-						<div style="font-family:impact; font-size:1.7em; margin:10px 0 10px 0;">{{strtoupper(date_format($record->created_at, "M"))}}</div>
+						<div style="font-family:impact; font-size:1.7em; margin:10px 0 10px 0;">@LANG('dt.' . strtoupper(date_format($record->created_at, "M")))</div>
 						<div style="font-family:impact; font-size:1.5em; margin-bottom:10px;">{{date_format($record->created_at, "j")}}</div>
 						<div>{{date_format($record->created_at, "Y")}}</div>
 					</div>
@@ -24,8 +24,8 @@
 				<td style="color:default; padding: 0 10px; text-align:left; padding:15px;">
 					<table>
 					<tbody>
-						<tr><td style="padding-bottom:10px; font-size:1.5em; font-weight:bold;">{{$record->name}}</td></tr>
-						<tr><td style="font-size: 1.3em; ">{{$record->comment}}</td></tr>
+						<tr><td style="padding-bottom:10px; font-size:1.3em; font-weight:bold;">{{$record->name}}</td></tr>
+						<tr><td style="font-size: 1em; ">{{$record->comment}}</td></tr>
 					</tbody>
 					</table>
 				</td>
--- resources/views/frontpage/index.blade.php
+++ resources/views/frontpage/index.blade.php
@@ -1005,7 +1005,7 @@ box-shadow: 0px 1px 4px 0px rgba(0,0,0,0.1);
 		<tr class="drop-box" style="vertical-align:middle; box-shadow: 2px 2px 2px 2px rgba(0, 0, 0, 0.2), 0 1px 1px 0 rgba(0, 0, 0, 0.19);">
 			<td style="min-width:100px; font-size: 1.5em; padding:10px; color: white; background-color: #327ab6; margin-bottom:10px;" >
 				<div style="margin:0; padding:0; line-height:100%;">
-					<div style="font-family:impact; font-size:1.7em; margin:10px 0 10px 0;">{{strtoupper(date_format($record->created_at, "M"))}}</div>
+					<div style="font-family:impact; font-size:1.7em; margin:10px 0 10px 0;">@LANG('dt.' . strtoupper(date_format($record->created_at, "M")))</div>
 					<div style="font-family:impact; font-size:1.5em; margin-bottom:10px;">{{date_format($record->created_at, "j")}}</div>
 					<div>{{date_format($record->created_at, "Y")}}</div>
 				</div>
@@ -1013,8 +1013,8 @@ box-shadow: 0px 1px 4px 0px rgba(0,0,0,0.1);
 			<td style="background-color:#f1f1f1; padding: 0 10px; text-align:left; padding:15px;">
 				<table>
 				<tbody>
-					<tr><td style="color: #327ab6; padding-bottom:10px; font-size:1.5em; font-weight:bold;">{{$record->name}}</td></tr>
-					<tr><td style="font-size: 1.3em; "><a href="/comments">{{$record->comment}}</a></td></tr>
+					<tr><td style="color: #327ab6; padding-bottom:10px; font-size:1.3em; font-weight:bold;">{{$record->name}}</td></tr>
+					<tr><td style="font-size: 1em; "><a href="/comments">{{$record->comment}}</a></td></tr>
 				</tbody>
 				</table>
 			</td>
