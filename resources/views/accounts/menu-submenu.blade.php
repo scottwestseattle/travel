a89cfd0ca304abbd619ba0a7edb0755d2eda5cee
scottwestseattle <sbwilkinson@yahoo.com>
2020-05-01 21:56:28 -0700

    Added transaction submenu items to accounts view

--- resources/views/accounts/menu-submenu.blade.php
+++ resources/views/accounts/menu-submenu.blade.php
@@ -3,12 +3,15 @@
 		<table><tr>
 			<td><a href="#" onclick="window.history.back()"><span style="font-size: 23px" class="glyphCustom glyphicon glyphicon-circle-arrow-left"></span></a></td>
 			<td><a href='/transactions/summary/'><span class="glyphCustom glyphicon glyphicon-home"></span></a></td>
+			@if (null !== session('transactionFilter'))
+			<td><a href='/transactions/filter/'><span class="glyphCustom glyphicon glyphicon-filter"></span></a></td>
+			@endif			
+			<td><a href='/transactions/filter/'><span class="glyphCustom glyphicon glyphicon-list"></span></a></td>
 			<td><a href='/{{$prefix}}/add/'><span class="glyphCustom glyphicon glyphicon-plus-sign"></span></a></td>			
 			<td style="font-size:12px;"><a style="padding:1px 4px; margin:5px 5px 9px 0px;" class="btn btn-primary" href="/{{$prefix}}/index">Visible</a></td>
 			<td style="font-size:12px;"><a style="padding:1px 4px; margin:5px 5px 9px 0px;" class="btn btn-primary" href="/{{$prefix}}/index/nonzero">Active</a></td>
 			<td style="font-size:12px;"><a style="padding:1px 4px; margin:5px 5px 9px 0px;" class="btn btn-primary" href="/{{$prefix}}/index/all">All</a></td>
 
-			
 			@if (isset($record->id))
 				<td><a href='/{{$prefix}}/view/{{$record->id}}'><span class="glyphCustom glyphicon glyphicon-eye-open"></span></a></td>
 				<td><a href='/{{$prefix}}/edit/{{$record->id}}'><span class="glyphCustom glyphicon glyphicon-edit"></span></a></td>
